e98771f4370fb0a8e0e30d5b6aea27e04bb46d93
ZagAlex1 <zagor84@bk.ru>
2022-07-06 16:12:23 +0300

    Lesson8

---
+++ src/main/java/ru/geekbrains/lessons/lesson8/Listeners/TestButtonListener.java
@@ -0,0 +1,11 @@
+package ru.geekbrains.lessons.lesson8.Listeners;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TestButtonListener implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println(e);
+    }
+}
